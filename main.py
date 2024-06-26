f4b19da38824b8f6147b1516cdee95ae92777b2c
Antraxmin <antraxmin@naver.com>
2024-06-26 12:51:09 +0900

    add trash category counts

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ async def predict(file: UploadFile = File(...)):
 
     detections = results[0].boxes
     count = len(detections)
+
+    label_counts = {}
     
     for box in detections:
         x1, y1, x2, y2 = box.xyxy[0]
@@ -36,6 +38,11 @@ async def predict(file: UploadFile = File(...)):
         score = box.conf[0] 
 
         label_text = label_map.get(label, str(label))
+
+        if label_text in label_counts:
+            label_counts[label_text] += 1
+        else:
+            label_counts[label_text] = 1
 
         cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
 
@@ -51,6 +58,7 @@ async def predict(file: UploadFile = File(...)):
 
     return JSONResponse(content={
         "count": count,
+        "label_counts": label_counts,
         "image": img_base64
     })
 
